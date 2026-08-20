71026c1c1ea07fbb198d12f08d426827e14d07b3
Thiago-Navo <Thiagonavo7105@gmail.com>
2026-08-19 22:14:47 -0300

    feat(templates): adiciona novos templates HTML e recursos estáticos
    - Adiciona templates para login, cadastro, dashboard, produtos, entradas e saídas
    - Adiciona scripts JS (app.js, nav.js, playlist.js) e estilos CSS na pasta static
    - Inclui suporte a visualização de estoque e status em produtos.html

---
+++ app/static/js/playlist.js
@@ -0,0 +1,39 @@
+const PLAYLIST = [
+
+  {
+    titulo: "2_23 AM",
+    artista: "Sharou",
+    src: "/static/audio/2_23 AM.mp3",
+    capa: "/static/img/covers/music.png",
+  },
+
+{
+    titulo: "303 PM",
+    artista: "Sharou",
+    src: "/static/audio/303 PM.mp3",
+    capa: "/static/img/covers/music.png",
+  },
+
+{
+    titulo: "INFP - しゃろう (youtube)",
+    artista: "Sharou",
+    src: "/static/audio/INFP - しゃろう (youtube).mp3",
+    capa: "/static/img/covers/music.png",
+  },
+
+{
+    titulo: "superstar",
+    artista: "Sharou",
+    src: "/static/audio/superstar.mp3",
+    capa: "/static/img/covers/music.png",
+  },
+
+{
+    titulo: "サンタは中央線でやってくる",
+    artista: "Sharou",
+    src: "/static/audio/サンタは中央線でやってくる.mp3",
+    capa: "/static/img/covers/music.png",
+  },
+
+];
+
