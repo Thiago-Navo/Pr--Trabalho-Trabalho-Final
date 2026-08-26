810750554d4ca1474177146973a87c3444eeb0cb
mauriciokeiser <keiser.mauricio@gmail.com>
2026-08-26 19:00:27 -0300

    Arquivo alterado: __init__.py
    Arquivo alterado para importar o registro de controle e do registro do Blueprint da API no arquivo adicionado no local: app/controllers/api_controller

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
 from app.controllers.main_controller import front_bp
-
+from app.controllers.api_controller import api_bp # Registro do Controller da API - POR DEV MAURICIO
 
 def create_app() -> Flask:
     app = Flask(__name__, static_folder="css")
     app.register_blueprint(front_bp)
+    app.register_blueprint(api_bp) # Registro do Blueprint da API - POR DEV MAURICIO
 
     return app
