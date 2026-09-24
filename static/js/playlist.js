1e23849aa99f830b3f5cc08925c18529de227e4c
Thiago Rodrigues Barbosa <Thiagonavo7105@gmail.com>
2026-09-23 21:44:20 -0300

    melhorias

--- static/js/playlist.js
+++ static/js/playlist.js
@@ -1,10 +1,53 @@
 const PLAYLIST = [
 
   {
-    titulo: "Adicione sua primeira faixa",
+    titulo: "Primeira faixa",
     artista: "Sharou",
     src: "/static/audio/INFP - しゃろう (youtube).mp3",
     capa: "/static/img/covers/music.png",
   },
+
+  {
+    titulo: "Segunda faixa",
+    artista: "Sharou",
+    src: "/static/audio/サンタは中央線でやってくる.mp3",
+    capa: "/static/img/covers/music1.png",
+  },
+
+  {
+    titulo: "Terceira faixa",
+    artista: "Sharou",
+    src: "/static/audio/superstar.mp3",
+    capa: "/static/img/covers/music2.png",
+  },
+  
+  {
+    titulo: "Quarta faixa",
+    artista: "Sharou",
+    src: "/static/audio/quarta.mp3",
+    capa: "/static/img/covers/music3.png",
+  },
+
+  {
+    titulo: "Quinta faixa",
+    artista: "Sharou",
+    src: "/static/audio/303 PM.mp3",
+    capa: "/static/img/covers/music4.png",
+  },
+
+   {
+    titulo: "Sexta faixa",
+    artista: "Sharou",
+    src: "/static/audio/2_23 AM.mp3",
+    capa: "/static/img/covers/music5.png",
+  },
+
+  {
+    titulo: "Sétima faixa",
+    artista: "Sharou",
+    src: "/static/audio/Sétima.mp3",
+    capa: "/static/img/covers/music6.png",
+  },
+
 ];
 
